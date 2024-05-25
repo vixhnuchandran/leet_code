61ab08279011bc5de8db1189d27212c9dcae04ad
vixhnuchandran <vixhnuchandran@gmail.com>
2024-05-25 22:43:40 +0530

    Solved

---
+++ database/181.Employees-Earning-More-Than-Their-Managers.sql
@@ -0,0 +1,26 @@
+
+SELECT e1.name 'Employee'
+FROM employee e1
+INNER JOIN employee e2 ON
+e1.managerid=e2.id WHERE e1.salary > e2.salary;
+
+
+
+-- Table: Employee
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+-- Write a solution to find the employees who earn more than their managers.
+
+-- Return the result table in any order.
+
---
+++ database/182.Duplicate-Emails.sql
@@ -0,0 +1,21 @@
+SELECT email from Person
+GROUP BY email
+HAVING COUNT(email)>1;
+
+
+
+-- Table: Person
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+-- Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+-- Return the result table in any order.
